9aa90b7bc0d9c77ec25b6d34c8b38f8db49dd896
Johann Og <kronolynx@gmail.com>
2016-08-02 18:09:31 +0200

    Implement API login

--- example.rb
+++ example.rb
@@ -1,20 +1,20 @@
 require_relative 'index'
 
-stampery = Stampery.new('367c6ec2-5791-4cf5-8094-4bae00c639b4', 'prod')
+stampery = Stampery.new('f7862a3e-174e-4741-f882-60702952d335', 'prod')
 
-stampery.on :proof do |hash, proof|
-    puts("Received proof for #{hash}");
-    puts proof.inspect
-    stampery.prove(hash, proof)
-end
-
-stampery.on :error do |err|
-    puts "Woot: #{err}"
-end
-
-stampery.on :ready do
-    digest = stampery.hash("Hello, blockchain!")
-    res = stampery.stamp(digest)
-end
+# stampery.on :proof do |hash, proof|
+#     puts("Received proof for #{hash}");
+#     puts proof.inspect
+#     stampery.prove(hash, proof)
+# end
+#
+# stampery.on :error do |err|
+#     puts "Woot: #{err}"
+# end
+#
+# stampery.on :ready do
+#     digest = stampery.hash("Hello, blockchain!")
+#     res = stampery.stamp(digest)
+# end
 
 stampery.start()
--- index.rb
+++ index.rb
@@ -8,19 +8,24 @@ require "event_emitter"
 
 class Stampery
     include EventEmitter
-    @apiEndpoints = {'prod' => ['api.stampery.com', 4000],
+    @@apiEndpoints = {'prod' => ['api.stampery.com', 4000],
                      'beta' => ['api-beta.stampery.com', 4000]}
-    @amqpEndpoints = {'prod' => ['young-squirrel.rmq.cloudamqp.com', 5672, 'consumer', '9FBln3UxOgwgLZtYvResNXE7', 'ukgmnhoi'],
+    @@amqpEndpoints = {'prod' => ['young-squirrel.rmq.cloudamqp.com', 5672, 'consumer', '9FBln3UxOgwgLZtYvResNXE7', 'ukgmnhoi'],
                       'beta' => ['young-squirrel.rmq.cloudamqp.com', 5672, 'consumer', '9FBln3UxOgwgLZtYvResNXE7', 'beta']}
 
     def initialize(secret, branch = 'prod')
-        @clientId = Digest::MD5.hexdigest(secret[0,15])
+        @clientSecret = secret
+        @clientId = Digest::MD5.hexdigest(secret)[0,15]
+        @apiEndpoint = @@apiEndpoints[branch] || @@apiEndpoints['prod']
     end
 
     def start
+        apiLogin @apiEndpoint
+
     end
 
-    def stamp
+    def stamp hash
+        puts "Stamping #{hash}"
     end
 
     def hash data
@@ -29,7 +34,10 @@ class Stampery
 
     private
     def apiLogin endpoint
-
+        apiClient = MessagePack::RPC::Client.new(endpoint[0], endpoint[1])
+        req = apiClient.call_async('stampery.3.auth', @clientId, @clientSecret)
+        req.join
+        @auth = req.result
     end
 
     def amqpLogin endpoint
