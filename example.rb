14676df910afc9141b73f239a22c7dba684d2134
Johann Og <kronolynx@gmail.com>
2016-08-03 14:28:46 +0200

    Implement amqpLogin and consume queue

--- example.rb
+++ example.rb
@@ -1,20 +1,18 @@
 require_relative 'index'
 
-stampery = Stampery.new('f7862a3e-174e-4741-f882-60702952d335', 'prod')
+stampery = Stampery.new('2d4cdee7-38b0-4a66-da87-c1ab05b43768', 'prod')
 
-# stampery.on :proof do |hash, proof|
-#     puts("Received proof for #{hash}");
-#     puts proof.inspect
-#     stampery.prove(hash, proof)
-# end
-#
-# stampery.on :error do |err|
-#     puts "Woot: #{err}"
-# end
-#
-# stampery.on :ready do
-#     digest = stampery.hash("Hello, blockchain!")
-#     res = stampery.stamp(digest)
-# end
+stampery.on :proof do |hash, proof|
+    puts("\nReceived proof for \n#{hash} \n\n Proof\n#{proof}");
+end
+
+stampery.on :error do |err|
+    puts "Woot: #{err}"
+end
+
+stampery.on :ready do
+    digest = stampery.hash("Hello, blockchain!")
+    stampery.stamp(digest)
+end
 
 stampery.start()
--- index.rb
+++ index.rb
@@ -1,14 +1,13 @@
 require "bunny"
 require 'digest/md5'
 require 'sha3'
-#require 'msgpack'
 require 'msgpack/rpc'
 require "event_emitter"
 
 
 class Stampery
     include EventEmitter
-    @@apiEndpoints = {'prod' => ['api.stampery.com', 4000],
+    @@apiEndpoints = {'prod' => ['192.168.1.52', 4000],
                      'beta' => ['api-beta.stampery.com', 4000]}
     @@amqpEndpoints = {'prod' => ['young-squirrel.rmq.cloudamqp.com', 5672, 'consumer', '9FBln3UxOgwgLZtYvResNXE7', 'ukgmnhoi'],
                       'beta' => ['young-squirrel.rmq.cloudamqp.com', 5672, 'consumer', '9FBln3UxOgwgLZtYvResNXE7', 'beta']}
@@ -17,30 +16,54 @@ class Stampery
         @clientSecret = secret
         @clientId = Digest::MD5.hexdigest(secret)[0,15]
         @apiEndpoint = @@apiEndpoints[branch] || @@apiEndpoints['prod']
+        @amqpEndpoint = @@amqpEndpoints[branch] || @@amqpEndpoints['prod']
     end
 
     def start
         apiLogin @apiEndpoint
-
+        amqpLogin @amqpEndpoint
     end
 
     def stamp hash
-        puts "Stamping #{hash}"
+        puts "Stamping \n#{hash}"
+        begin
+            @apiClient.call('stamp', hash.upcase)
+        rescue Exception => e
+            emit :error, e
+        end
     end
 
     def hash data
-        SHA3::Digest.hexdigest(:sha224, data).upcase
+        SHA3::Digest.hexdigest(:sha512, data).upcase
     end
 
     private
     def apiLogin endpoint
-        apiClient = MessagePack::RPC::Client.new(endpoint[0], endpoint[1])
-        req = apiClient.call_async('stampery.3.auth', @clientId, @clientSecret)
+        @apiClient = MessagePack::RPC::Client.new(endpoint[0], endpoint[1])
+        req = @apiClient.call_async('stampery.3.auth', @clientId, @clientSecret)
         req.join
         @auth = req.result
+        puts "logged #{@clientId} Auth #{@auth}"
     end
 
     def amqpLogin endpoint
+        amqpConn = Bunny.new(:automatically_recover => true, :host => endpoint[0], :port => endpoint[1], :user => endpoint[2], :pass => endpoint[3], :vhost => endpoint[4] )
+        amqpConn.start
+        puts '[QUEUE] Connected to Rabbit!'
+        emit :ready
 
+        amqpChannel = amqpConn.create_channel
+
+        queue = amqpChannel.queue(@clientId + '-clnt', :no_declare => true)
+        begin
+            queue.subscribe(:block => true) do |delivery_info, _metadata, queueMsg|
+                hash = delivery_info.routing_key
+                proof = MessagePack.unpack queueMsg
+                emit :proof ,hash ,proof
+            end
+        rescue Exception => _
+            puts "\n\nClosing the client"
+        end
+        #
     end
 end
